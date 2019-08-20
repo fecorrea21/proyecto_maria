bef33ce15f8ac450ceaef662ac00783fb2de0012
fecorrea <fcorrea@omnibnk.com>
2019-08-20 17:34:17 -0500

    dia_4

--- vistas/frame_proveedor.py
+++ vistas/frame_proveedor.py
@@ -55,10 +55,63 @@ class Vista_proveedor(ttk.Frame):
         self.tabla.heading('#2', text="TELEFONO")
         self.tabla.heading('#3', text="DIRECCION")
 
-        """Boton Editar"""
-        self.boton_editar = Button(self, text="EDITAR PROVEEDOR")
-        self.boton_editar.grid(row=7, column=0, pady=10, padx=10)
-
         """Boton Eliminar"""
         self.boton_eliminar = Button(self, text="ELIMINAR PROVEEDOR")
-        self.boton_eliminar.grid(row=7, column=1, pady=10, padx=10)
+        self.boton_eliminar.grid(row=7, column=0, pady=10, padx=10)
+
+        """Editar datos"""
+
+        def editar_datos():
+            """Arranque de ventana Editar"""
+            self.ventana_editar = Toplevel()
+            self.ventana_editar.title("EDITAR PROVEEDOR")
+
+            """Label y Campo de Codigo"""
+            self.label_codigo = Label(self.ventana_editar, text="Codigo Proveedor: ")
+            self.label_codigo.grid(row=0, column=0, pady=10, padx=10)
+            self.entry_codigo = Entry(self.ventana_editar, state='readonly')
+            self.entry_codigo.grid(row=0, column=1, pady=10, padx=10)
+
+            """Label y Campo de Antiguo Nombre"""
+            self.label_antiguo_nombre = Label(self.ventana_editar, text="Antiguo Nombre Proveedor: ")
+            self.label_antiguo_nombre.grid(row=1, column=0, pady=10, padx=10)
+            self.entry_antigua_nombre = Entry(self.ventana_editar, state='readonly')
+            self.entry_antigua_nombre.grid(row=1, column=1, pady=10, padx=10)
+
+            """Label y Campo de Nuevo Nombre"""
+            self.label_nuevo_nombre = Label(self.ventana_editar, text="Nuevo Nombre Proveedor: ")
+            self.label_nuevo_nombre.grid(row=2, column=0, pady=10, padx=10)
+            self.entry_nuevo_nombre = Entry(self.ventana_editar)
+            self.entry_nuevo_nombre.grid(row=2, column=1, pady=10, padx=10)
+
+            """Label y Campo de Antiguo Telefono"""
+            self.label_antiguo_telefono = Label(self.ventana_editar, text="Antiguo Nombre Telefono: ")
+            self.label_antiguo_telefono.grid(row=3, column=0, pady=10, padx=10)
+            self.entry_antigua_telefono = Entry(self.ventana_editar, state='readonly')
+            self.entry_antigua_telefono.grid(row=3, column=1, pady=10, padx=10)
+
+            """Label y Campo de Nuevo Telefono"""
+            self.label_nuevo_telefono = Label(self.ventana_editar, text="Nuevo Nombre Telefono: ")
+            self.label_nuevo_telefono.grid(row=4, column=0, pady=10, padx=10)
+            self.entry_nuevo_telefono = Entry(self.ventana_editar)
+            self.entry_nuevo_telefono.grid(row=4, column=1, pady=10, padx=10)
+
+            """Label y Campo de Antigua Direccion"""
+            self.label_antigua_direccion = Label(self.ventana_editar, text="Antigua Direccion Proveedor: ")
+            self.label_antigua_direccion.grid(row=5, column=0, pady=10, padx=10)
+            self.entry_antigua_duracion = Entry(self.ventana_editar, state='readonly')
+            self.entry_antigua_duracion.grid(row=5, column=1, pady=10, padx=10)
+
+            """Label y Campo de Nueva Direccion"""
+            self.label_nueva_direccion = Label(self.ventana_editar, text="Nueva Direccion Proveedor: ")
+            self.label_nueva_direccion.grid(row=6, column=0, pady=10, padx=10)
+            self.entry_nueva_duracion = Entry(self.ventana_editar)
+            self.entry_nueva_duracion.grid(row=6, column=1, pady=10, padx=10)
+
+            """Boton Actualizar"""
+            self.boton_actualizar = Button(self.ventana_editar, text="ACTUALIZAR")
+            self.boton_actualizar.grid(row=7, column=0, columnspan=2, pady=10, padx=10)
+
+        """Boton Editar"""
+        self.boton_editar = Button(self, text="EDITAR PROVEEDOR", command= editar_datos)
+        self.boton_editar.grid(row=7, column=1, pady=10, padx=10)
